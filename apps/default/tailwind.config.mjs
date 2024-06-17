b7bd572dec7136c52eb051a0b28a955d08009e80
Franklin Shera <fshera96@gmail.com>
2024-06-17 12:04:21 +0300

    add colors

--- apps/default/tailwind.config.mjs
+++ apps/default/tailwind.config.mjs
@@ -13,6 +13,27 @@ export default {
         inter: ["Inter Variable", ...dt.fontFamily.serif],
         sora: ["Sora Variable", ...dt.fontFamily.serif],
       },
+      colors: {
+        brand: {
+          dark: {
+            1: "#1c1e1d",
+            2: "#121212",
+            3: "#171717",
+            4: "#1a1a1a",
+            5: "#212121",
+          },
+          gray: {
+            1: "#353535",
+            2: "#cacaca",
+            3: "#5c5c5c",
+            4: "#525252",
+          },
+          green: {
+            1: "#006c2e",
+            2: "#c1fdda",
+          },
+        },
+      },
     },
   },
   plugins: [],
