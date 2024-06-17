023fe7190dcb066dbc41b8e3d9f21781d26c769a
Franklin Shera <fshera96@gmail.com>
2024-06-17 12:12:21 +0300

    add site config

---
+++ apps/default/src/config/index.ts
@@ -0,0 +1,13 @@
+export const site = {
+  title: "Realty",
+  address: {
+    street: "123 Main Street",
+    city: "Denver",
+    state: "CO",
+    zip: "80202",
+  },
+  contact: {
+    phoneNumbers: ["+333-3-666999", "+999-6-333396"],
+    email: "info@yourinfo.com",
+  },
+};
